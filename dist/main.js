7c39ffad495984d33fc04d7976c544fe42fb04e3
gianlucacolotto <68969900+gianlucacolotto@users.noreply.github.com>
2021-03-19 00:22:05 +0100

    First commit
    First example of node files

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+console.log("This is my first Node Project!");
+var myVar = 3;
+myVar += 10; // 13
+console.log('myVar = ', myVar);
+var myInterface = {
+    myNumber: 10,
+    myString: 'Hello World'
+};
+console.log('myNumber = ', myInterface.myNumber);
+console.log('myString = ', myInterface.myString);
---
+++ main.ts
@@ -0,0 +1,21 @@
+console.log("Hello World!")
+console.log("This is my first Node Project!")
+
+let myVar: number = 3;
+
+myVar += 10; // 13
+
+console.log('myVar = ', myVar);
+
+interface MyInterface {
+    myNumber: number;
+    myString: string;
+}
+
+let myInterface: MyInterface = {
+    myNumber: 10,
+    myString: 'Hello World'
+};
+
+console.log('myNumber = ', myInterface.myNumber)
+console.log('myString = ', myInterface.myString)
