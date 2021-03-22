5624d8b8987349b679169ad628975090e61548cb
gianlucacolotto <68969900+gianlucacolotto@users.noreply.github.com>
2021-03-23 00:21:12 +0100

    Lezioni successive
    Modifiche successive

--- main.ts
+++ main.ts
@@ -1,21 +1,224 @@
-console.log("Hello World!")
-console.log("This is my first Node Project!")
+var myVar: string = 'ciao';
 
-let myVar: number = 3;
+var myVar2: string;
+myVar2 = 'ciao';
 
-myVar += 10; // 13
+let myVar3: string = 'ciao';
 
-console.log('myVar = ', myVar);
+const myConstant = 'ciao';
 
-interface MyInterface {
-    myNumber: number;
-    myString: string;
+let myNumber: number = 10;
+
+let myBoolean :boolean = true;
+
+let myNumber2: number | undefined;
+console.log('myNumber2 = ', myNumber2);
+
+let myObject: object =  {};
+
+let anyObj: any;
+anyObj = 'ciao';
+anyObj = 10;
+anyObj = {
+    ad: 10
 }
 
-let myInterface: MyInterface = {
-    myNumber: 10,
-    myString: 'Hello World'
+let myList : number[] = [12, 13, 14]
+let myStringList: string[] = ['ciao','a','b'];
+
+let myTuple : [string, number] = ['ciao',10];
+
+
+for(let x = 0; x < 10; x= x + 1 ){
+    console.log('Ciclo for - Numero: ', x);
+}
+
+const numbers = [2, 4, 6, 8];
+for(const x of numbers){
+    console.log('Ciclo foreach - Numero: ', x);
+}
+
+
+let x = 1;
+while (x < 10){
+    console.log('Ciclo while - numero: ', x);
+    x = x +1;
+}
+
+
+const first_numbers = [1,2,3,4,5];
+let sum: number = 0;
+
+for(let index: number = 0; index < 5; index = index + 1){
+    sum = sum + first_numbers[index];
+}
+
+console.log("La somma è : ", sum);
+
+console.log("La media è : ", sum/first_numbers.length);
+
+let nameToPrint: string = hello('Gian Luca');
+console.log("La stringa di ritorno è: " + nameToPrint);
+
+console.log(greeting('Gian Luca'));
+console.log(greeting('Gian Luca','en'));
+console.log(greeting('Gian Luca','es'));
+
+//--------------------
+
+//LAMBDA FUNCTION
+
+const random = (min: number, max: number) => {
+    return Math.random() * (max-min) + min;
 };
 
-console.log('myNumber = ', myInterface.myNumber)
-console.log('myString = ', myInterface.myString)
+console.log('random number between 3 and 5: ', random(3, 5));
+
+const minimo = (arr: number[]) : number => {
+
+  let min = arr[0];
+  for(let n of arr){
+      if(n < min){
+          min = n;
+      }
+  }
+  return min;
+}
+
+const massimo = (arr: number[]) : number => {
+
+    let max = arr[0];
+    for(let n of arr){
+        if(n > max){
+            max = n;
+        }
+    }
+    return max;
+  }
+
+//---------------------
+
+let arrayOfNumbers : number[] = [1,2,3,4,5];
+
+console.log('La somma vale: ',  sum_array(arrayOfNumbers));
+console.log('La media vale : ', media_array(sum_array(arrayOfNumbers), arrayOfNumbers.length));
+
+
+function sum_array(listToAdd : number[]): number{
+    
+    let incrementalSum: number = 0;
+
+    for(let index: number = 0; index < 5; index = index + 1){
+        incrementalSum = incrementalSum + listToAdd[index];
+    }
+
+    return incrementalSum;
+
+}
+
+function media_array(sumTotal : number, length: number){
+
+    return sumTotal / length;
+
+}
+
+
+//-------------------------
+
+
+function hello(name: string): string {
+    console.log('Hello world!');
+    console.log('Hello from ', name);
+    return name;
+}
+
+
+function greeting (name: string, lang: "en" | 'it' | 'es' = 'it'){
+
+const greetMap = {
+    en: 'Hello',
+    it: 'Ciao',
+    es: 'Hola',
+}
+
+const greetingWord = greetMap[lang];
+
+return `${greetingWord} ${name}`;
+
+}
+
+
+
+//----------------------
+
+//FUNZIONI DI ORDINE SUPERIORE (FUNZIONI CHE HANNO IN INGRESSO
+//ALTRE FUNZIONI)
+
+function printFunction (func: () => void){
+    console.log('calling function');
+    func();
+    console.log('function called');
+}
+
+printFunction( () => {
+    console.log(greeting('Gian Luca'));
+} );
+
+
+const numbersToTest: number[] = [1, 2, 3, 4, 5];
+
+const squaredNumbers = numbersToTest.map( n => n*n);
+const oddNumbers = numbersToTest.filter(n => n%2 != 0).map(n => n + 10);
+
+console.log('Squared numbers', squaredNumbers);
+console.log('Odd numbers: ', oddNumbers);
+
+
+
+/* Compilare codice implementato in typescrit in javascript in modo
+*da poterlo distribuire in ambiente node senza bisogno di installare typescript,
+*quindi per poter renderlo un pò più veloce.
+*/
+
+//Programma scritto in typescript quindi non automaticamente leggibile da Node Js
+//oppure dal browser; i browser non capiscono direttamente typescript ma javascript.
+
+/*
+* ts-node è un wrapper typescript che da la capacità a Node JS di leggere codice nativo
+* typescript, solitamente non vogliamo, perché ts-node è un più più lento perché il parser 
+* deve leggere un lungaggio un pò più complesso di javascript
+* Typescript ci mette a disposizione tsc: comando che permette di compilare producendo un file sotto
+* la directory dist contenente il codice javascript.
+* Una volta fatto questo occorre invocare tale codice con node seguito dal file
+* contenente il codice javascript;
+* Ts-node è infatti più lento perché al suo interno prima compila il codice in javascript
+  e poi invoca node su di esso 
+*/
+
+
+/* DIPENDENZE IN NODE
+* npm (node packet manager) = sistema per installare e gestire pacchetti esterni che altri utenti 
+* hanno già rilasciato all'interno della repository
+* Es. faker = libreria per generare dati finti di alcune persone (mokare) che usano l'applicazione 
+* senza chiedere a decine, centinaia di persone di usarla.
+*
+* npm i faker = npm install faker => 
+* 1) file package.json contiene nuova sezione dedicata alle dependencies con faker.
+* 2) viene generato file package-lock.json importante per mantenere la funzionalità
+* di tutte le dipendenze che installiamo, in particolare per gestire le sottodipendenze
+* delle librerie e deve essere committata all'interno della nostra repository.
+* 
+* faker non contiene tipi typescript per utilizzarla, cioè typescript non trova i tipi
+* per poterlo utilizzare: risulta errore se scriviamo
+* import faker from 'faker'
+* Soluzione: npm install @types/faker per installare le dipendenze. 
+* Quasi tutte le librerie funzionano in questom modo.
+* Da installare con opzione --save-dev ad indicare che non si tratta di una libreria
+* di sistema da includere nel bundle finale javascript, ma che serve solo per sviluppo.
+* Se andiamo nel file package.json, dependencies saranno le dipendenze che andranno ufficialmente
+* nel pacchetto che noi distribuiamo; devDependencies sono le dipendenze che ci sono
+* solamente a scopo di sviluppo.
+*
+* Node js = back end
+* browser = front-end
+*/
